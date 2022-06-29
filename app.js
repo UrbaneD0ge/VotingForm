8678f4ea7b7e3461d50f86b5858513a961658611
Kip Dunlap <kdunlap@atlantaga.gov>
2022-06-29 13:42:02 -0400

    fiddling with grid columns, can't generate row

--- app.js
+++ app.js
@@ -25,25 +25,49 @@ const table = document.getElementById('table');
 // document.querySelector('#itmType').addEventListener('change', preFill);
 
 // on submit, add form data to table
-submit.addEventListener('submit', (e) => {
+submit.addEventListener('click', (e) => {
   e.preventDefault();
   // header inputs
-let NPU = document.getElementById('NPU').value || '';
+let NPU = document.getElementById('NPU').selectedOptions[0].value || '';
 let chair = document.querySelector('#chair').value || '';
 let loc = document.querySelector('#location').value || '';
 let planner = document.querySelector('#planner').value || '';
 let date = document.querySelector('#date').value || '';
 // // Add Item form
-let itmType = document.querySelector('#itmType').value || '';
+let itmType = document.querySelector('#itmType').selectedOptions[0].value || '';
 let applName = document.querySelector('#applName').value || '';
 let disposal = document.querySelector('#disposal').value || '';
 let comments = document.querySelector('#conditions').value || '';
 
-  let newRow = document.innerHTML = `<tr> <td>${NPU}</td> <td>${chair}</td> <td>${loc}</td> <td>${planner}</td> <td>${date}</td> <td>${itmType}</td> <td>${applName}</td> <td>${disposal}</td> <td>${comments}</td> </tr>`;
-    // append new row to table
-    table.appendChild(newRow);
-    console.log('new row added');
-    // clear inputs
-    document.querySelector('#addItem').reset();
+  // create table row
+  let row = document.createElement('tr');
+  // create table cells
+  let itmTypeCell = document.createElement('td');
+  let applNameCell = document.createElement('td');
+  let disposalCell = document.createElement('td');
+  let commentsCell = document.createElement('td');
+  // add text to cells
+  itmTypeCell.textContent = itmType;
+  applNameCell.textContent = applName;
+  disposalCell.textContent = disposal;
+  commentsCell.textContent = comments;
+
+  // append new row to table
+  table.appendChild(row);
+
+  // append cells to row
+  row.appendChild(itmTypeCell);
+  row.appendChild(applNameCell);
+  row.appendChild(disposalCell);
+
+  if (comments !== '') {
+    // create new row for comments
+    table.appendChild(row);
+    row.appendChild(commentsCell);
+  }
+
+  console.log('new row added');
+  // clear inputs
+  document.querySelector('#addItem').reset();
     }
 );
